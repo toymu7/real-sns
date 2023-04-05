b884b32aee3942f47b5232dcbb8113462420b609
toymu7 <mut7ttrs9@outlook.jp>
2023-04-05 21:40:39 +0900

    DB接続

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const userRoute = require("./routes/users.js");
 const authRoute = require("./routes/auth.js");
 const postRoute = require("./routes/posts.js");
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// データベース接続
+mongoose.connect(
+  process.env.MONGOURL
+).then(() => {
+  console.log("DBと接続中");
+}).catch((err) => {
+  console.log(err);
+});
 
 // ミドルウェア
 app.use("/api/users", userRoute);
