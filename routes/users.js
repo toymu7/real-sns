ac00e4bc6e64f10b919ccc7a64effa7fab22dc4b
toymu7 <mut7ttrs9@outlook.jp>
2023-04-07 20:00:31 +0900

    ユーザー情報削除

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,20 @@ router.put("/:id", async(req, res) => {
   }
 });
 
+// ユーザー情報の削除
+router.delete("/:id", async(req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin){
+    try{
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が削除されました");
+    }catch(err){
+      console.log(err);
+    }
+  }else{
+    return res.status(403).json("あなたは自分のアカウントの時のみ情報を削除できます。");
+  }
+});
+
 
 // router.get("/", (req, res) => {
 //   res.send("user router");
