e9223570aea909cfa246dbc0dbd7bb6994b73486
toymu7 <mut7ttrs9@outlook.jp>
2023-04-07 19:53:27 +0900

    ユーザー情報更新

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
+const User = require("../models/User.js");
 
-
+// CRUD
+// ユーザー情報の更新
+router.put("/:id", async(req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin){
+    try{
+      const user = await User.findByIdAndUpdate(req.params.id, {$set: req.body});
+      res.status(200).json("ユーザー情報が更新できました");
+    }catch(err){
+      console.log(err);
+    }
+  }else{
+    return res.status(403).json("あなたは自分のアカウントの時のみ情報を更新できます。");
+  }
+});
 
 
 // router.get("/", (req, res) => {
