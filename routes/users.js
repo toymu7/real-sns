7b76d8afa6c335c334e25c2e5cdf4328fbf32679
toymu7 <mut7ttrs9@outlook.jp>
2023-04-08 11:26:39 +0900

    ユーザーフォロー機能

--- routes/users.js
+++ routes/users.js
@@ -41,6 +41,37 @@ router.get("/:id", async(req, res) => {
     }
 });
 
+// ユーザのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id){
+    try{
+      // 今からフォローするユーザ
+      const user = await User.findById(req.params.id);
+      // 自分
+      const currentUser = await User.findById(req.body.userId);
+      // フォロワーに自分がいなければフォローできる
+      if (!user.followers.includes(req.body.userId)){
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId
+          },
+        });
+        await currentUser.updateOne({
+          $push:{
+            followings:req.params.id,
+          },
+        });
+        return res.status(200).json("フォローに成功しました。");
+      }else{
+        return res.status(403).json("あなたは既にこのユーザーをフォローしています。")
+      }
+    }catch(err){
+      console.log(err);
+    }
+  }else{
+    return res.status(500).json("自分自身をフォローできません。")
+  }
+});
 
 
 module.exports = router;
