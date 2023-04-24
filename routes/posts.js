5ac0662e703098e07b33ffe0ade04cf678be7810
toymu7 <mut7ttrs9@outlook.jp>
2023-04-24 21:24:14 +0900

    投稿を更新する処理

--- routes/posts.js
+++ routes/posts.js
@@ -10,7 +10,23 @@ router.post("/", async (req, res) => {
   }catch(err){
     return res.status(500).json(err);
   }
-
 });
+
+// 投稿を更新する
+router.put("/:id", async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId){
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿編集に成功しました");
+    }else{
+      return res.status(403).json("あなたは他の人の投稿を編集できません");
+    }
+  }catch(err){
+    res.status(403).json(err);
+  };
+})
 
 module.exports = router;
